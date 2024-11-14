d8c8f075d159abd857b22f13d32b16671327f42a
Jia Tan <jiatanoffice@gmail.com>
2024-11-14 09:29:01 +0600

    playGame func added

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
+function playGame(playerMove) {
+    const computerMove = pickComputerMove()
+    
+
+    let result = ''
+    if (playerMove === 'Rock'){
+        if (computerMove === 'Rock') {
+            result = 'Tie'
+        } else if (computerMove === 'Paper') {
+            result = 'You Lose'
+        } else if (computerMove === 'Scissors') {
+            result = 'You Win'
+        } 
+        
+        
+    } else if (playerMove === 'Paper'){
+        if (computerMove === 'Rock') {
+            result = 'You Win'
+        } else if (computerMove === 'Paper') {
+            result = 'Tie'
+        } else if (computerMove === 'Scissors') {
+            result = 'You Lose'
+        }
+    } else if (playerMove === 'Scissors'){
+        if (computerMove === 'Rock') {
+            result = 'You Lose'
+        } else if (computerMove === 'Paper') {
+            result = 'You Win'
+        } else if (computerMove === 'Scissors') {
+            result = 'Tie'
+        }
+    }
+
+
+    alert('You picked ' + playerMove + ', Computer picked '+ computerMove + ', Result is: ' + result)
+
+    /*
+
+    -- Another way to make this alert --
+    alert(`You picked Rock, Computer picked ${computerMove}, Result is: ${result}`)
+    
+    */
+
+
+}
+
+
+
 function pickComputerMove() {
     const randomNumber = Math.random()
     let computerMove = ''
